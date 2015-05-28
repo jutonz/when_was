a48f9122048b0af93da496a043c66d173c1ac646
Justin Toniazzo <jutonz42@gmail.com>
2015-05-28 12:51:42 -0400

    Fix: Determines airdates for episodes 701+

--- spec/when_was_spec.rb
+++ spec/when_was_spec.rb
@@ -21,6 +21,24 @@ describe WhenWas do
       expect(airdate).to eq expected
     end
 
+    it 'knows when episode 701 was' do
+      airdate = WhenWas.ASOT 701
+      expected = Date.new 2015, 2, 19
+      expect(airdate).to eq expected
+    end
+
+    it 'knows when episode 702 was' do
+      airdate = WhenWas.ASOT 702
+      expected = Date.new 2015, 2, 26
+      expect(airdate).to eq expected
+    end
+
+    it 'knows when episode 714 was' do
+      airdate = WhenWas.ASOT 714
+      expected = Date.new 2015, 5, 21
+      expect(airdate).to eq expected
+    end
+
   end
 
 end
